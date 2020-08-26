8a887d43e8e5e2fcb7997822fa9b6c6a4aeb20f5
SarahAlAshwal <sa.alashwal@gmail.com>
2020-08-26 19:14:52 +0000

    refactoring and creating index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,17 @@
-const catName = process.argv[2];
 const request = require('request');
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${catName}`;
-request(url,(error,response,body) => {
+const fetchBreedDescription = function(breedName, callback) {
 
-  if (error) {
-    console.log(error);
-    return;
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-});
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url,(error,response,body) => {
+
+    if (error) {
+      callback(error,null);
+    } else {
+      const data = JSON.parse(body);
+      callback(null,data[0].description);
+      
+    }
+  });
+};
+module.exports = {fetchBreedDescription};
